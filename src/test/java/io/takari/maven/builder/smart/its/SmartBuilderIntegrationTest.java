f1118652d8d890b7b448a5db0e0472305c190d03
Igor Fedorenko <igor@ifedorenko.com>
2014-12-18 11:58:21 -0500

    run integration tests with maven 3.2.5
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- src/test/java/io/takari/maven/builder/smart/its/SmartBuilderIntegrationTest.java
+++ src/test/java/io/takari/maven/builder/smart/its/SmartBuilderIntegrationTest.java
@@ -16,7 +16,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(MavenJUnitTestRunner.class)
-@MavenVersions({"3.2.1", "3.2.2", "3.2.3", "3.2.4-SNAPSHOT"})
+@MavenVersions({"3.2.1", "3.2.2", "3.2.3", "3.2.5"})
 public class SmartBuilderIntegrationTest {
 
   @Rule
